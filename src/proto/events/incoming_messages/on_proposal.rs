d48406c4a3fa91da7fca1a8c25ca8df8185776a4
Andrey Sobol <andreysobol@protonmail.com>
2022-11-10 07:50:30 +0100

    on_proposal

--- src/proto/events/incoming_messages/on_proposal.rs
+++ src/proto/events/incoming_messages/on_proposal.rs
@@ -1,15 +1,28 @@
+use crate::messages::message::Message;
 use crate::proto::consensus_state::ConsensusState;
 use crate::messages::proposal::Proposal;
+use crate::proto::predicates::proposal_validity::proposal_validty;
 
 pub fn on_proposal(
     consensus_state: ConsensusState,
     proposal: Proposal,
-) -> ConsensusState {
+) -> (ConsensusState, Option<Message>) {
+
+    let mut new_consensus_state = consensus_state.clone();
+    let mut message = None;
+
     if consensus_state.proposal.is_none() {
-        let mut new_consensus_state = consensus_state.clone();
-        new_consensus_state.proposal = Some(proposal);
-        new_consensus_state
-    } else {
-        consensus_state
+
+        if proposal_validty(
+            proposal.clone(),
+            consensus_state.round,
+            consensus_state.height,
+            &consensus_state.validators,
+        ) {
+            new_consensus_state.proposal = Some(proposal.clone());
+            message = Some(Message::Proposal(proposal));
+        }
     }
+    
+    (new_consensus_state, message)
 }
