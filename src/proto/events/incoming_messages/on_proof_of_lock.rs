e121b156067a17a47d13ae7e821c08b45fb286f8
Andrey Sobol <andreysobol@protonmail.com>
2022-11-10 07:22:03 +0100

    return message

--- src/proto/events/incoming_messages/on_proof_of_lock.rs
+++ src/proto/events/incoming_messages/on_proof_of_lock.rs
@@ -9,6 +9,7 @@ pub fn on_proof_of_lock(
 ) -> (ConsensusState, Option<Message>) {
 
     let mut new_consensus_state = consensus_state.clone();
+    let mut message = None;
 
     if consensus_state.locked_state.is_none() {
         let proof_of_lock_clone = proof_of_lock.clone();
@@ -23,9 +24,10 @@ pub fn on_proof_of_lock(
             let state_transition = proof_of_lock.proposal.state_transition;
             let new_locked_state = state_transition.apply_state_transition(&consensus_state.state);
             new_consensus_state.locked_state = Some(new_locked_state);
-            new_consensus_state.proof_of_lock = Some(proof_of_lock_clone);
+            new_consensus_state.proof_of_lock = Some(proof_of_lock_clone.clone());
+            message = Some(Message::ProofOfLock(proof_of_lock_clone));
         }
     }
 
-    (new_consensus_state, None)
+    (new_consensus_state, message)
 }
