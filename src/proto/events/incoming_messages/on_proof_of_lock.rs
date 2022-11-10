fe9322bf0cc31a5718bbfaad6b1963173c48f58d
Andrey Sobol <andreysobol@protonmail.com>
2022-11-10 07:08:47 +0100

    on_proof_of_lock refactor

--- src/proto/events/incoming_messages/on_proof_of_lock.rs
+++ src/proto/events/incoming_messages/on_proof_of_lock.rs
@@ -10,24 +10,20 @@ pub fn on_proof_of_lock(
 
     let mut new_consensus_state = consensus_state.clone();
 
-    match consensus_state.locked_state {
-        Some(locked_state) => {}
-        None => {
+    if consensus_state.locked_state.is_none() {
+        let proof_of_lock_clone = proof_of_lock.clone();
 
-            let proof_of_lock_clone = proof_of_lock.clone();
-
-            if proof_of_lock_validity(
-                &proof_of_lock,
-                consensus_state.round,
-                consensus_state.height,
-                &consensus_state.validators,
-                consensus_state.threshold
-            ) {
-                let state_transition = proof_of_lock.proposal.state_transition;
-                let new_locked_state = state_transition.apply_state_transition(&consensus_state.state);
-                new_consensus_state.locked_state = Some(new_locked_state);
-                new_consensus_state.proof_of_lock = Some(proof_of_lock_clone);
-            }
+        if proof_of_lock_validity(
+            &proof_of_lock,
+            consensus_state.round,
+            consensus_state.height,
+            &consensus_state.validators,
+            consensus_state.threshold
+        ) {
+            let state_transition = proof_of_lock.proposal.state_transition;
+            let new_locked_state = state_transition.apply_state_transition(&consensus_state.state);
+            new_consensus_state.locked_state = Some(new_locked_state);
+            new_consensus_state.proof_of_lock = Some(proof_of_lock_clone);
         }
     }
 
